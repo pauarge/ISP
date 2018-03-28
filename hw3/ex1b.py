42565195352f2628dc697eda3594725c5d216e6d
Pau Argelaguet <pau@outlook.com>
2018-03-28 18:32:54 +0200

    Ex 2a done

---
+++ hw3/ex1b.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+names = ['email', 'movie', 'date', 'rating']
+
+
+def main():
+    imdb = pd.read_csv("data/imdb-2.csv", names=names, quotechar='"', skipinitialspace=True)
+    dedis = pd.read_csv("data/dedis-2.csv", names=names, quotechar='"', skipinitialspace=True)
+
+    grouped_movies_imdb = [imdb.iloc[v] for _, v in imdb.groupby(['movie']).groups.items()]
+    grouped_movies_dedis = [dedis.iloc[v] for _, v in dedis.groupby(['movie']).groups.items()]
+
+    grouped_movies_imdb.sort(key=lambda x: len(x), reverse=True)
+    grouped_movies_dedis.sort(key=lambda x: len(x), reverse=True)
+
+    movies = {}
+    me = ""
+
+    for x, y in zip(grouped_movies_imdb, grouped_movies_dedis):
+        movies[y.iloc[0].movie] = x.iloc[0].movie
+
+    for i, m in dedis.iterrows():
+        dedis.at[i, 'movie'] = movies[dedis.at[i, 'movie']]
+
+    grouped_users_imdb = [imdb.iloc[v] for _, v in imdb.groupby(['email']).groups.items()]
+    grouped_users_dedis = [dedis.iloc[v] for _, v in dedis.groupby(['email']).groups.items()]
+
+    for it, i in enumerate(grouped_users_imdb):
+        print("Processing {} of {}".format(it, len(grouped_users_imdb)))
+        for d in grouped_users_dedis:
+            m = i.merge(d, on=['movie', 'rating']).drop_duplicates()
+            if len(m) == len(i.drop_duplicates()):
+                if m.iloc[0].email_x == 'pau.argelaguet@epfl.ch':
+                    print("My username is {}".format(m.iloc[0].email_y))
+                    me = m.iloc[0].email_y
+                break
+
+    my_movies = dedis.loc[dedis['email'] == me]['movie']
+    for i, m in my_movies.iteritems():
+        my_movies.at[i] = movies[my_movies.at[i]]
+
+    my_movies.to_csv("data/my-movies-2.csv", index=False)
+
+
+if __name__ == '__main__':
+    main()
---
+++ hw3/ex2a.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"os"
+	"bufio"
+	"fmt"
+	"sync"
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+func nextPassword(n int, c string) func() string {
+	r := []rune(c)
+	p := make([]rune, n)
+	x := make([]int, len(p))
+	return func() string {
+		p := p[:len(x)]
+		for i, xi := range x {
+			p[i] = r[xi]
+		}
+		for i := len(x) - 1; i >= 0; i-- {
+			x[i]++
+			if x[i] < len(r) {
+				break
+			}
+			x[i] = 0
+			if i <= 0 {
+				x = x[0:0]
+				break
+			}
+		}
+		return string(p)
+	}
+}
+
+func testpasswords(n int, wg *sync.WaitGroup, passwds []string) {
+	np := nextPassword(n, "abcdefghijklmnopqrstvuwxyz0123456789")
+	for {
+		pwd := np()
+		if len(pwd) == 0 {
+			break
+		}
+		hasher := sha256.New()
+		hasher.Write([]byte(pwd))
+		res := hex.EncodeToString(hasher.Sum(nil))
+		if stringInSlice(res, passwds) {
+			fmt.Println(pwd)
+		}
+	}
+	wg.Done()
+}
+
+func main() {
+	fileHandle, _ := os.Open("/Users/pauargelaguet/Development/isp/hw3/data/hw3_ex2.txt")
+	defer fileHandle.Close()
+	fileScanner := bufio.NewScanner(fileHandle)
+
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	var passwds []string
+
+	for i := 0; i < 11; i++ {
+		fileScanner.Scan()
+		passwds = append(passwds, fileScanner.Text())
+	}
+
+	go testpasswords(4, &wg, passwds)
+	go testpasswords(5, &wg, passwds)
+	go testpasswords(6, &wg, passwds)
+
+	wg.Wait()
+
+	fmt.Println("Done")
+}
